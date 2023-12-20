61477d623c0e34eab5ee3cdb882b09105d251b97
Caroline Rousseau <caroline.rousseau410@gmail.com>
2023-12-20 16:24:52 +0100

    Modification de la partie User pour homogénaisation du code
    + modif sur User

--- src/main/java/edu/m2i/api_gestion_bibliotheque/service/GestionUserService.java
+++ src/main/java/edu/m2i/api_gestion_bibliotheque/service/GestionUserService.java
@@ -5,11 +5,14 @@ import java.util.List;
 import edu.m2i.api_gestion_bibliotheque.dto.UserDTO;
 import edu.m2i.api_gestion_bibliotheque.entity.User;
 
-public interface UserService {
-	
+public interface GestionUserService {
+
 	UserDTO save(User user);
+
 	UserDTO findById(Integer id);
+
 	List<User> findAll();
+
 	void delete(Integer id);
 
 }
