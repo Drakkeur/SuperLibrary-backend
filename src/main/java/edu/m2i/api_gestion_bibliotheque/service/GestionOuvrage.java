050fca2dc6364c455540d35661d030cd0ed02300
Subnumine <verneuil.j@outlook.com>
2023-12-20 15:16:00 +0100

    modif_Jeremy_15h

--- src/main/java/edu/m2i/api_gestion_bibliotheque/service/GestionOuvrage.java
+++ src/main/java/edu/m2i/api_gestion_bibliotheque/service/GestionOuvrage.java
@@ -2,11 +2,14 @@ package edu.m2i.api_gestion_bibliotheque.service;
 
 import java.util.List;
 
+import edu.m2i.api_gestion_bibliotheque.DTO.OuvrageDTO;
 import edu.m2i.api_gestion_bibliotheque.entity.Ouvrage;
 
 public interface GestionOuvrage {
 	
 	public List<Ouvrage> getOuvrage();
+	public List<Ouvrage> getOuvrage(String filter);
+	public OuvrageDTO getOuvrage(Integer id);
 	public void save(Ouvrage ouvrage);
 	public void deleteOuvrage(Integer id);
 }
--- src/main/java/edu/m2i/api_gestion_bibliotheque/service/imp/GestionOuvrageImp.java
+++ src/main/java/edu/m2i/api_gestion_bibliotheque/service/imp/GestionOuvrageImp.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import edu.m2i.api_gestion_bibliotheque.DTO.LoanDTO;
 import edu.m2i.api_gestion_bibliotheque.DTO.OuvrageDTO;
-import edu.m2i.api_gestion_bibliotheque.entity.Loan;
 import edu.m2i.api_gestion_bibliotheque.entity.Ouvrage;
 import edu.m2i.api_gestion_bibliotheque.repository.OuvrageRepository;
 import edu.m2i.api_gestion_bibliotheque.service.GestionOuvrage;
@@ -17,25 +15,28 @@ public class GestionOuvrageImp implements GestionOuvrage{
 	
 	@Override
 	public List<Ouvrage> getOuvrage(){
-		
+		return ouvrageRepository.findAll();
 	}
 	
 	@Override
 	public OuvrageDTO getOuvrage(Integer id){
 		Ouvrage ouvrage = ouvrageRepository.getReferenceById(id);
 		OuvrageDTO ouvrageDTO = new OuvrageDTO();
-		ouvrageDTO.setDateStart(loan.getDateStart());
-		ouvrageDTO.setTheoreticalDateEnd(loan.getTheoreticalDateEnd());
-		ouvrageDTO.setRealDateEnd(loan.getRealDateEnd());
-		ouvrageDTO.setStatus(loan.getStatus());
-		ouvrageDTO.setOuvrage(loan.getOuvrage());
-		ouvrageDTO.setUser(loan.getUser());
+		ouvrageDTO.setCote(ouvrage.getCote());
+		ouvrageDTO.setTitle(ouvrage.getTitle());
+		ouvrageDTO.setMainAuthor(ouvrage.getMainAuthor());
+		ouvrageDTO.setOtherAuthor(ouvrage.getOtherAuthor());
+		ouvrageDTO.setGenre(ouvrage.getGenre());
+		ouvrageDTO.setEditor(ouvrage.getEditor());
+		ouvrageDTO.setPublishedDate(ouvrage.getPublishedDate());
+		ouvrageDTO.setComment(ouvrage.getComment());
+		ouvrageDTO.setAvailability(ouvrage.getAvailability());
 		return ouvrageDTO;
 	}
 	
 	@Override
-	public List<Ouvrage> getOuvrage(String title){
-		
+	public List<Ouvrage> getOuvrage(String filter){
+		return ouvrageRepository.getOuvrage(filter);
 	}
 	
 	@Override
